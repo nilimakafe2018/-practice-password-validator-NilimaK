731cabc1a9d4c0b5384cfa0e08319c5c63eb3655
nilima kafle <nilimakafle2018@gmail.com>
2025-07-02 16:47:42 -0500

    PasswordValidatorCompleted1

--- PasswordValidator.js
+++ PasswordValidator.js
@@ -1,24 +1,23 @@
 const readlineSync = require('readline-sync');
 
-let Mypassword = '';
-let isValid = false;
+let Mypassword = ''; 
+let isValid = false; 
 
-console.log("Welcome! Please follow the requirements while creating your password. \n1. The password must be at least 8 characters long. \n2. The password must contain at least one uppercase letter. \n3. The password must contain at least one number.")
+console.log("Welcome! Please follow the requirements while creating your password. \n1. The password must be at least 8 characters long. \n2. The password must contain at least one uppercase letter. \n3. The password must contain at least one number from 0 to 9.")
 
 while (!isValid) {
  password = readlineSync.question("Please enter your password,:");
 
-
 //Checking the length of the password.
- if (password.length < 8) { // if the length is less than 8
-   console.log("Your password must be at least 8 characters long.");
+ if (password.length < 8) { // checking if the length is less than 8 or not 
+   console.log("Your password must be at least 8 characters long."); 
    continue; //asking again
  }
 
  // Checking if the passwork has at least 1 uppercase letter or not
  let hasUppercase = false;
  for (let i = 0; i < password.length; i++) {
-   if (password[i] >= 'A' && password[i] <= 'Z') { //checking if the password has uppercase A-Z
+   if (password[i] >= 'A' && password[i] <= 'Z') { //checking if the password has uppercase A-Z or not
      hasUppercase = true;
      break;
    }
