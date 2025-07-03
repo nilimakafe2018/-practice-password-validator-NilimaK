4f56aec65ce0c6e848ea420759b43ce1425fac9b
nilima kafle <nilimakafle2018@gmail.com>
2025-07-03 15:53:37 -0500

    LatestCommit

--- PasswordValidator.js
+++ PasswordValidator.js
@@ -1,23 +1,23 @@
 const readlineSync = require('readline-sync');
 
-let Mypassword = ''; 
+let NewPassword = ''; 
 let isValid = false; 
 
 console.log("Welcome! Please follow the requirements while creating your password. \n1. The password must be at least 8 characters long. \n2. The password must contain at least one uppercase letter. \n3. The password must contain at least one number from 0 to 9.")
 
 while (!isValid) {
-  Mypassword = readlineSync.question("Please enter your password,:");
+  NewPassword = readlineSync.question("Please enter your password,:");
 
 //Checking the length of the password.
- if (Mypassword.length < 8) { // checking if the length is less than 8 or not 
+ if (NewPassword.length < 8) { // checking if the length is less than 8 or not 
    console.log("Your password must be at least 8 characters long."); 
    continue; //asking again
  }
 
  // Checking if the passwork has at least 1 uppercase letter or not
  let hasUppercase = false;
- for (let i = 0; i < Mypassword.length; i++) {
-   if (Mypassword[i] >= 'A' && Mypassword[i] <= 'Z') { //checking if the password has uppercase A-Z or not
+ for (let i = 0; i < NewPassword.length; i++) {
+   if (NewPassword[i] >= 'A' && NewPassword[i] <= 'Z') { //checking if the password has uppercase A-Z or not
      hasUppercase = true;
      break;
    }
@@ -29,8 +29,8 @@ while (!isValid) {
 
  // Checking if there is any number or not
  let hasNumber = false;
- for (let i = 0; i < Mypassword.length; i++) {
-   if (Mypassword[i] >= '0' && Mypasswordclear[i] <= '9') { //checking if the number is between 0 to 9 or not
+ for (let i = 0; i < NewPassword.length; i++) {
+   if (NewPassword[i] >= '0' && NewPassword[i] <= '9') { //checking if the number is between 0 to 9 or not
      hasNumber = true;
      break;
    }
